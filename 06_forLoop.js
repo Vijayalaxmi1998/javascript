6c0a87e800f6144aeae095fa1bc638c0d5ce50c9
Vijayalaxmi Kaleguddi <vkaleguddi@gmail.com>
2024-01-05 09:34:40 +0530

    learning Loop and iterative Statement

---
+++ 06_forLoop.js
@@ -0,0 +1,30 @@
+//0 to 10 Numbers Print
+for(var index = 0;index<=10;index++){
+    console.log(index);
+}
+
+console.log("2, 4,6,8,10 Numbers to print");
+//2, 4,6,8,10 Numbers to print
+for(let index = 2;index<=10;index=index+2){
+    console.log(index);
+}
+
+console.log("========================Number from 10 to 1================================");
+for(let index=10;index>=1;index--){
+    console.log(index);
+}
+
+console.log("====================Number from 50 to 40===========================");
+for(i=50;i>=40;i--){
+    console.log(i);
+}
+
+console.log("===========WAP to print the table of 5===========================");
+for(let i=5;i<=50;i=i+5){
+   console.log(i);
+}
+
+console.log(`========= Example of Infinite loop==============`);
+// for (let index = 5; index < 10; index--) {
+//    console.log(index); // 5 4 3 2 1 0 ----------
+// }
