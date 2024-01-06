5c839fa533f5eef3df685a05802b7c2aed2fff6f
Vijayalaxmi Kaleguddi <vkaleguddi@gmail.com>
2024-01-06 18:58:07 +0530

    created scope.j file

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,68 @@
+console.log("STEP 1\n");
+console.log("Print number from 5 to 50 by incrementing 1");
+var i=5;
+while(i<=15){
+    console.log(i);
+    i++;
+}
+
+console.log("-------------------------------\n\nSTEP 2\n");
+console.log("Print numbers from 50 to 40 by decrementing by 1");
+var i=50;
+while (i>=40) {
+    console.log(i);
+    i--
+    
+}
+console.log("-------------------------------\n\nSTEP 3\n");
+console.log("First 15 odd numbers");
+var i=1;
+var count=1;
+while(count<=15){
+    console.log(i);
+    i=i+2;
+    count++;
+}
+
+console.log("-------------------------------\n\nSTEP 4\n");
+console.log("First 10 Even Number");
+i=0;
+count=1;
+while (count<=10) {
+    console.log(i);
+    i+=2;
+    count++;
+    
+}
+
+console.log("-------------------------------\n\nSTEP 5\n");
+console.log("Print table of 5 like -> 5 10 15 20 ... ... 50");
+i=5;
+while (i<=50) {
+    console.log(i);
+    i+=5;
+    
+}
+console.log("-------------------------------\n\nSTEP 6\n");
+console.log("Print table of 10 like -> 10 20 30 40 ... ... 100");
+
+i=10;
+while (i<=100) {
+    console.log(i);
+    i=i+10;
+    
+}
+
+console.log("-------------------------------\n\nSTEP 6\n");
+console.log("Print table of 10 in reverse order like -> 100 90 80 70 ... ... 10");
+
+i=100;
+while (i>=10) {
+    console.log(i);
+    i=i-10;
+    
+}
+
+console.log("**************************************************");
+
+
