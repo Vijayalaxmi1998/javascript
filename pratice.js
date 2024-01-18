7e571ffcba8d1034219ed2f3afc930ca08210bb4
Vijayalaxmi Kaleguddi <vkaleguddi@gmail.com>
2024-01-18 23:17:37 +0530

    pratice.js
    study about loop

--- pratice.js
+++ pratice.js
@@ -1,68 +1,76 @@
-console.log(`This is a globel scope Variable`);
 
-var a=10;
-let firstName="Vijayalaxmi";
-const pi=3.14;
-function detail() {
-    console.log(`a=${firstName}, firstName:${firstName}, value of pi=${3.14}`);
+const add = (n1,n2,n3,n4,n5) => {
+    return n1 + n2 + n3 + n4 + n5;
+}
+const returnValue = add(100,100,200,349,756);
+console.log(`Addition is: ${returnValue}`);
+const returnValue1 = add("I am"," learning ","ES6 ","feature ","in depth");
+console.log(`Addition is: ${returnValue1}`);
+
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+    
+}
+console.log(`----------------------------------------------------`);
+for (let index = 50; index >= 40; index--) {
+    console.log(index);
+    
 }
 
-detail()
-console.log(firstName);
-console.log(a);
-console.log(pi);
-
-console.log(`\n\nfunction scope Variable`);
-
-
-
-function detail() {
-    var a1=10;
-let firstName1="Vijayalaxmi";
-const pi1=3.14;
-   console.log(`a=${firstName}, firstName:${firstName}, value of pi=${3.14}`);
+console.log(`----------------------------------------------------`);
+for (let index = 0; index <=15; index++) {
+    if (index%2==1) {
+        console.log(index);
+    }
+    
 }
 
-detail();
-// console.log(firstName1);
-// console.log(a1);
-// console.log(pi1);
-
-console.log(`\n\nblock scope Variable`);
-var b=10;
-if(b==10){
-    var a3=10;
-let firstName3="Vijayalaxmi";
-const pi3=3.14;
-console.log(`a=${firstName}, firstName:${firstName}, value of pi=${3.14}`);
-
-
+console.log(`----------------------------------------------------`);
+for (let index = 5; index <= 50; index++) {
+    if (index%5==0) {
+        console.log(index);
+    }
+    
 }
 
-//console.log(firstName3);
-console.log(a3);  // hence var function scope
-//console.log(pi3);
-
-class Person{
-name;
-age;
-
-constructor(name,age){
-    this.name=name;
-    this.age=age;
-}
-play(){
-    console.log("He Love Playing Criket");
-}
-eat(){
-    console.log("he likes Pizza");
-}
+console.log(`----------------------------------------------------`);
+for (let index = 100; index >=10 ; index--){
+    if(index%10==0){
+        console.log(index);
+    }  
 }
 
-let anilPerson=new Person("anil",25);
-let vijayaPerson=new Person("vijaya",26);
-console.log(anilPerson);
-console.log(vijayaPerson);
+console.log(`----------------------------------------------------`);
+console.log(`--------------Using While Loop--------------------------------------`);
+var i=5;
+while (i<=15) {
+    console.log(i);
+    i++;
+    
+}
 
-console.log(anilPerson.name);
-anilPerson.play();
+console.log(`----------------------------------------------------`);
+var i=50;
+while (i>=40) {
+    
+    console.log(i);
+    i--;
+}
+
+console.log(`----------------------------------------------------`);
+
+function vowels(str){
+    for (var index = 0; index < str.length; index++) {
+        if (str[index]=="a"||str[index]=="e"||str[index]=="i"||str[index]=="o"||str[index]=="u") {
+         console.log(str[index]);   
+        }
+        
+    }
+}
+vowels("vaishali");
+
+console.log(`----------------------------------------------------`);
+
+
+
+
