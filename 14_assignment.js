45e3706ee5360cd2366e6c057023033ca47a8427
Vijayalaxmi Kaleguddi <vkaleguddi@gmail.com>
2024-01-21 22:07:34 +0530

    Update 14_assignment.js

--- 14_assignment.js
+++ 14_assignment.js
@@ -9,14 +9,33 @@ function removeDuplicate(arr) {
 }
 
 removeDuplicate([11,3,4,11,4,7,3])
+let array=[1,2,3,4,5,8];
+console.log(array.indexOf(8));
+console.log(array.includes(100));
+let array1=[];
+console.log(array1.indexOf(11));
+console.log(array.indexOf(111));
 
-// const str= "How are you mate"
+console.log(`\n------------------------------------------------------------------------------------------------`);
 
-//Expected output => "How ArE YoU MatE";
-
-function capital(str){
-  let words
+function lastFirstCharCapital(str) {
+  let words=str.split(" ");
+  for(let i=0; i < words.length; i++){
+    let first=words[i][0];
+    let last= words[i][words[i].length-1];
+    first=first.toUpperCase();
+    last=last.toUpperCase();
+    words[i]=first+words[i].slice(1,-1)+last;
+   // console.log(words[i]);
+  
+  }
+  words= words.join(" ");
+  return words;
 }
+let sentence="how are you mate";
+console.log(`Given word is: ${sentence}`);
+let afterChange=lastFirstCharCapital(sentence);
+console.log(`\nAfter using Function: ${afterChange}`);
 
 
 
